cf35e656a202f48930628b07aa145171d9933fb4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-12 19:24:30 +0800

    添加armvirt armv8架构的openwrt 2024-06-12

--- patch/N1.sh
+++ patch/N1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #设置斐讯N1的rootfs.tar.gz路径并下载
-OWRT_PATH="https://github.com/rmoyulong/N1-OpenWrt/releases/download/s905d_openwrt_Latest/openwrt-armvirt-64-generic-rootfs.tar.gz"
+OWRT_PATH="https://github.com/rmoyulong/openwrt-armar-armv8/releases/download/s905d_openwrt_Latest/openwrt-armsr-armv8-generic-rootfs.tar.gz"
 curl -L -o output/openwrt-armvirt-64-generic-rootfs.tar.gz $OWRT_PATH
